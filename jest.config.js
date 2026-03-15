d4b769d8455847db2ea83bc65ea4648f935a76ec
Dorcas Adetunji <adetunjidorcas7@gmail.com>
2026-03-15 05:13:06 +0000

    chore: add Vercel cron schedule and Jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: { module: "commonjs" } }],
+  },
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  clearMocks: true,
+};
+
+module.exports = config;
